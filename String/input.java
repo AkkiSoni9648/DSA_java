990a9ad0cb16743bdc17b459175fe836df92eae2
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-02-02 15:33:59 +0530

    string

---
+++ String/input.java
@@ -0,0 +1,13 @@
+package String;
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your name: ");
+   // String a=sc.next();//only one string 
+    String b =sc.nextLine ();//whole string
+   // System.out.println("Hii"+a);
+        System.out.println("hii"+b);
+sc.close();
+    }
+}
