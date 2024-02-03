c9417ac68e3d4466be91ae547d4157948aa5e3c1
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-02-03 10:45:47 +0530

    String

---
+++ String/indexof.java
@@ -0,0 +1,21 @@
+package String;
+
+public class indexof {
+    public static void main(String[] args) {
+        String s="Aakash Soni";
+        // accept the first one 
+        //if not found then give -1
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.indexOf('z'));
+        System.out.println(s.lastIndexOf('a'));
+     //2 given string compare  lexographically  
+        System.out.println();
+
+        String b="abc";
+        String c="dbc";
+        System.out.println(b.compareTo(c));
+
+        //97-100
+    }
+    
+}
