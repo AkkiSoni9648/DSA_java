89e496da8491c7ddb4d8179ac53d5a79e21cd63f
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-02-06 23:04:08 +0530

    String

---
+++ String/substring.java
@@ -0,0 +1,11 @@
+package String;
+
+public class substring {
+    public static void main(String[] args) {
+        String a="Akash Soni";
+        String b="Apoorv Soni";
+        System.out.println(a.substring(2));
+        System.out.println(a.substring(2,5));
+    }
+    
+}
