6cabeb57116992dd10ba38a530156385b74afe5e
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-02-06 22:54:52 +0530

    String

--- String/toLowercase_and_concat.java
+++ String/toLowercase_and_concat.java
@@ -3,6 +3,14 @@ package String;
 public class toLowercase_and_concat {
     public static void main(String[] args) {
         //convertion of string
+        String a="AKASH SONI";
+        //For one time
+        System.out.println(a.toLowerCase());
+        //permanent change
+       String s=a.toLowerCase();
+        System.out.println(s);
+        String b="Angel sharma";
+        System.out.println(a.concat(b));
     }
     
 }
