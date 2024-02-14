2498a76f8ac08b32403361a518812888b7b29401
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-02-14 20:25:45 +0530

    Sorting

--- basicSorting/check_issortedornot.java
+++ basicSorting/check_issortedornot.java
@@ -3,9 +3,10 @@ package basicSorting;
 public class check_issortedornot {
     public static void main(String[] args) {
         //sorting means arraning the data into decreasing or increasing order
+        //there are mainly three types of sorting
         int[]a={1,3,2,5,8,9};
         int n=a.length;
-        boolean flag=true;//true mans sorted
+        boolean flag=true;//true means sorted
         for (int i = 0; i < n-1; i++) {
             if(a[i]>a[i+1]){
                 flag=false;//false means unsorted
