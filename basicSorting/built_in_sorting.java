bdd8c9e1a9e35b08d092a611baded7ea4e60f30a
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-03-01 21:13:59 +0530

    sorting

--- basicSorting/built_in_sorting.java
+++ basicSorting/built_in_sorting.java
@@ -4,7 +4,7 @@ public class built_in_sorting {
     public static void main(String[] args) {
         int[]a={1,3,-2,5,8,9};
         for(int e:a){
-           
+           //sorting
         }
     }
     
