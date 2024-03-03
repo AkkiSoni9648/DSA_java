a93ce8efaf87c0c5abff171b7e3409a66f697295
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-03-03 18:58:53 +0530

    Method

---
+++ Method/what_is_method.java
@@ -0,0 +1,8 @@
+package Method;
+
+public class what_is_method {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- String/substring.java
+++ String/substring.java
@@ -6,7 +6,8 @@ public class substring {
         String b="Apoorv Soni";
         System.out.println(a.substring(2));
         //endindex is always execute n-1
-        System.out.println(a.substring(2,5));
+        //white space is also counted in indexing
+        System.out.println(a.substring(2,7));
 
     }
     
