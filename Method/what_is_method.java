733a0c686a8f0965169dc0c1e377f8c2c607a193
AkkiSoni9648 <akashsoni964893@gmail.com>
2024-03-07 21:34:21 +0530

    Method

--- Method/what_is_method.java
+++ Method/what_is_method.java
@@ -3,11 +3,12 @@ package Method;
 public class what_is_method {
     public static void main(String[] args) {
         //syntax of method
-       // name(parameter){
+       //  return_type name(parameter){
 //body of function
       // }
       // unstructured -mini project
       //structured -major project(method)
+
     }
     
 }
