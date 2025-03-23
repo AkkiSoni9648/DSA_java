859e3db6ce17aa0dd8b03595dfa1fe25f68f93c2
AkkiSoni9648 <akashsoni964893@gmail.com>
2025-03-23 23:40:34 +0530

    REcursion

---
+++ REcursion/Functioncall.java
@@ -0,0 +1,12 @@
+package REcursion;
+
+    public class Functioncall {
+        public static void apple() {
+            System.out.println("Apple");
+        }
+        public static void main(String[] args) {
+           System.out.println("HII i am in main method"); 
+            apple();
+            System.out.println("I am in the apple");
+        }
+    }
