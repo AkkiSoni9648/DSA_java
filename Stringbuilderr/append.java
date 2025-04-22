4fb3d94aa1a67ca9c0b38f724fe404f95fa6362b
AkkiSoni9648 <akashsoni964893@gmail.com>
2025-04-22 15:39:14 +0530

    Stringbuilderr

---
+++ Stringbuilderr/append.java
@@ -0,0 +1,13 @@
+package Stringbuilderr;
+import java.util.*;
+
+public class append {
+    public static void main(String[] args) {
+        StringBuilder a=new StringBuilder("akash");
+        System.out.println(a);
+        a.append(35);
+        System.out.println(a);
+        a.append("Soni");
+        System.out.println(a);
+    }//unable to add an arr if we want to add array then it gives its address.
+}
